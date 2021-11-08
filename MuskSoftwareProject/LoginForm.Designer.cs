3fa2f1451f15c69446f64ecd1a41fd55b6928068
Jabed Ahmed <JA978@student.aru.ac.uk>
2021-11-08 10:52:07 +0000

    First commit of the project

--- MuskSoftwareProject/LoginForm.Designer.cs
+++ MuskSoftwareProject/LoginForm.Designer.cs
@@ -51,7 +51,7 @@
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(384, 217);
+            this.btnLogin.Location = new System.Drawing.Point(397, 221);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(75, 23);
             this.btnLogin.TabIndex = 2;
