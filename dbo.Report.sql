8a0ae26408a3d374f8aa3fd6b750710e439213df
Beginner000000 <92723522+Beginner000000@users.noreply.github.com>
2021-12-12 20:30:35 +0000

    Report Table Created
    2nd table for Database

---
+++ dbo.Report.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Report]
+(
+	[Date] DATETIME NOT NULL PRIMARY KEY, 
+    [Report Description] TEXT NOT NULL, 
+    [Incident Level] TEXT NOT NULL, 
+    [Comments] TEXT NOT NULL
+)
