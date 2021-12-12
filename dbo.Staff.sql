e49b02b0d0d34ab1db6453b9336820cb485168e9
Beginner000000 <92723522+Beginner000000@users.noreply.github.com>
2021-12-12 20:35:42 +0000

    Revised Staff Table
    Problems regarding original version with issues regarding GitHub merge conflicts on Visual Studio 2019 which resulted in deletion of the original database.

---
+++ dbo.Staff.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Staff]
+(
+	[ Staff ID] INT NOT NULL PRIMARY KEY, 
+    [First Name] NCHAR(20) NOT NULL, 
+    [Last Name] NCHAR(20) NOT NULL, 
+    [Username] NVARCHAR(50) NOT NULL, 
+    [password] TEXT NOT NULL
+)
